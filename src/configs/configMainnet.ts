b6a5a3d07a12257efef6d637e4ab6d074c9d5af8
okanishcheva <okanishcheva@wavesplatform.com>
2023-05-16 16:33:02 +0400

    DEXW-6306: edit config

--- src/configs/configMainnet.ts
+++ src/configs/configMainnet.ts
@@ -1,7 +1,18 @@
 export default {
-    nodeUrl: "https://nodes.waves.exchange",
-    signerClientUrl: "https://waves.exchange/signer",
-    explorerUrl: "https://wavesexplorer.com",
-    dataServicesUrl: "https://waves.exchange/api/v1",
-    iconServiceUrl: "https://waves.exchange/static/icons/assets"
+    apiUrl: {
+        balance: 'https://nodes.wavesplatform.com/addresses/balance',
+        wavesBalance: 'https://nodes.wavesnodes.com/addresses/balance/details',
+        assets: 'https://wx.network/api/v1/assets',
+        assetsIcons: 'https://wx.network/static/icons/assets',
+        rates: 'https://wx.network/api/v1/rates',
+        nodeHeight: 'https://nodes.wavesplatform.com/blocks/height',
+    },
+    contracts: {
+
+    },
+    assets: [
+        { label: 'WAVES', id: 'WAVES' },
+        { label: 'WAVESDLP', id: '' },
+        { label: 'XTN', id: 'DG2xFkPdDwKUoBkzGAhQtLpSGzfXLiCYPEzeKH2Ad24p' },
+    ]
 }
