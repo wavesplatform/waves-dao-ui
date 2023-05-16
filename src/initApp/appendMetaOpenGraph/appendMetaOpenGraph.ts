b1b94f5599a7389612d676108aae827bc2a8ad31
okanishcheva <okanishcheva@wavesplatform.com>
2023-05-16 20:57:56 +0400

    DEXW-6321: add meta

---
+++ src/initApp/appendMetaOpenGraph/appendMetaOpenGraph.ts
@@ -0,0 +1,10 @@
+import openGraphConfig from './openGraphConfig';
+
+export const appendMetaOpenGraph = () : void => {
+	Object.entries(openGraphConfig).forEach(([property, content]) => {
+		const meta = document.createElement('meta');
+		meta.setAttribute('property', property);
+		meta.content= content;
+		document.getElementsByTagName('head')[0].appendChild(meta);
+	})
+}
