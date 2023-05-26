a5ed39da652e209aa5a9148b6ccae3eddfa324e9
Max Axenov <maksenov@wavesplatform.com>
2023-05-26 14:33:11 +0300

    parse contract data

--- src/stores/contractData/ContractDataStore.ts
+++ src/stores/contractData/ContractDataStore.ts
@@ -4,15 +4,32 @@ import { AppStore } from '../AppStore';
 import { FetchTracker } from '../utils/FetchTracker';
 import { search } from '../../utils/search/searchRequest';
 import { IState } from '../../utils/search';
+import { parseSearchStr } from '../../utils/search/parseContractData';
+import { Money } from '@waves/data-entities';
 
-// const CONTRACT_DATA_VALUES_MAP = [
-//     'startHeight': ['%s__startHeight'],
-//
+
+interface IWithdrawal {
+    status: string;
+    lpAssetAmount: Money;
+    targetPeriod: number;
+    claimTxId: string;
+}
+
+interface IContractData {
+    startHeight: number;
+    periodLength: number;
+    currentPeriod: number;
+    investedWaves: Money;
+    investedLp: Money;
+    availableToClaim: Money;
+    claimed: Money;
+    withdraws?: IWithdrawal[];
+}
 
 export class ContractDataStore extends ChildStore {
 
-    public contractData: FetchTracker<any, any>;
-    public priceData: FetchTracker<any, any>;
+    public contractData: FetchTracker<IContractData, IState>;
+    public priceData: FetchTracker<any, IState>;
 
     constructor(rs: AppStore) {
         super(rs);
@@ -34,6 +51,8 @@ export class ContractDataStore extends ChildStore {
                                     {
                                         or: [
                                             { key: { operation: 'eq', value: '%s__startHeight' } },
+                                            { key: { operation: 'eq', value: '%s__periodLength' } },
+                                            { key: { operation: 'eq', value: '%s__currentPeriod' } },
                                             { key: { operation: 'eq', value: '%s%s__invested__WAVES' } },
                                             { key: { operation: 'eq', value: `%s%s__invested__${'lPAssetId'}` } },
                                             { key: { operation: 'eq', value: `%s%s%s__available__${userAddress}` } },
@@ -90,15 +109,64 @@ export class ContractDataStore extends ChildStore {
         );
     }
 
-    private contractDataParser(data: IState): any {
-        const withdraws = [];
-        const parsedValues = data.entries.map((entry) => {
-            if (entry.key.includes('withdrawal')) {
-                withdraws.push(entry);
-                return null;
+    private contractDataParser(data: IState): IContractData {
+        const parseEntries = (key: string, value: string | number) => {
+            switch (true) {
+                case key.includes('startHeight'):
+                    return { startHeight: value };
+                case key.includes('periodLength'):
+                    return { periodLength: value };
+                case key.includes('currentPeriod'):
+                    return { currentPeriod: value };
+                case key.includes('invested__WAVES'):
+                    return { investedWaves: new Money(value, this.rs.assetsStore.assetsData.data.WAVES) };
+                case key.includes(`invested__${'lPAssetId'}`):
+                    return { investedLp: new Money(value, this.rs.assetsStore.assetsData.data['lPAssetId']) };
+                case key.includes('available'):
+                    return { availableToClaim: new Money(value, this.rs.assetsStore.assetsData.data['lPAssetId']) };
+                case key.includes('claimed'):
+                    return { claimed: new Money(value, this.rs.assetsStore.assetsData.data['lPAssetId']) };
+                case key.includes('withdrawal'):
+                    return { withdrawal: new Money(value, this.rs.assetsStore.assetsData.data['lPAssetId']) };
+                default:
+                    return {};
             }
-            // return parseSearchStr(entry.value, getContractValues(entry.key));
-        });
+        };
+
+        const parseWithdrawal = (entry) => {
+            // %s%d%d%s__<status:"PENDING"|"FINISHED">__<lpAssetAmount>__<targetPeriod>__<claimTxId|"SOON">
+            const parsedStr = parseSearchStr<{
+                status: string;
+                lpAssetAmount: number;
+                targetPeriod: number;
+                claimTxId: string;
+            }>(entry, [
+                'status',
+                'lpAssetAmount',
+                'targetPeriod',
+                'claimTxId'
+            ]);
+
+            return {
+                ...parsedStr,
+                lpAssetAmount: new Money(parsedStr.lpAssetAmount, this.rs.assetsStore.assetsData.data['lPAssetId'])
+            };
+        };
+
+        return data.entries.reduce((acc, entry) => {
+            if (entry.key.includes('withdrawal')) {
+                const parsedWithdrawal = parseWithdrawal(entry);
+                if (acc.withdraws) {
+                    acc.withdraws = [...acc.withdraws, parsedWithdrawal];
+                } else {
+                    acc.withdraws = [parsedWithdrawal];
+                }
+                return acc;
+            } else {
+                const parsed = parseEntries(entry.key, entry.value);
+                return { ...acc, ...parsed };
+            }
+        }, Object.create(null));
     }
 
 }
