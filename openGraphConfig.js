df5bb884ae57f3d51e453ce448c48265ae85f78a
Max Axenov <maksenov@wavesplatform.com>
2023-06-12 17:16:59 +0300

    edit meta

--- openGraphConfig.js
+++ openGraphConfig.js
@@ -1,17 +1,17 @@
 export default {
-  "og:type": "website",
-  "og:site_name": "WAVESDAO",
-  "og:title": "WAVESDAO",
-  "og:description": "DAO is making profit by financing the development of the projects and initiatives based on Waves blockchain.",
-  "og:url": "https://waves.dao",
-  "og:image": "https://waves.dao/WAVESDAO_social.png",
-  "og:image:width": 1400,
-  "og:image:height": 630,
-  "twitter:card": "summary_large_image",
-  "twitter:title": "WAVESDAO",
-  "twitter:description": "DAO is making profit by financing the development of the projects and initiatives based on Waves blockchain.",
-  "twitter:image:src": "https://waves.dao/WAVESDAO_social.png",
-  "twitter:image": "https://waves.dao/WAVESDAO_social.png",
-  "twitter:url": "https://waves.dao",
-  "vk:image": "https://waves.dao/WAVESDAO_social.png"
-}
+    'og:type': 'website',
+    'og:site_name': 'WAVESDAO',
+    'og:title': 'WAVESDAO',
+    'og:description': 'DAO is making profit by financing the development of the projects and initiatives based on Waves blockchain.',
+    'og:url': 'https://wavesdao.tech',
+    'og:image': 'https://wavesdao.tech/WAVESDAO_social.png',
+    'og:image:width': 1400,
+    'og:image:height': 630,
+    'twitter:card': 'summary_large_image',
+    'twitter:title': 'WAVESDAO',
+    'twitter:description': 'DAO is making profit by financing the development of the projects and initiatives based on Waves blockchain.',
+    'twitter:image:src': 'https://wavesdao.tech/WAVESDAO_social.png',
+    'twitter:image': 'https://wavesdao.tech/WAVESDAO_social.png',
+    'twitter:url': 'https://waves.dao',
+    'vk:image': 'https://wavesdao.tech/WAVESDAO_social.png'
+};
