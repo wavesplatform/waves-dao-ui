ae6c9a8b705f0e3c1ebf1b5d86e3085613d7a4f6
Olga Kanishcheva <okanishcheva@uniqsoft.ae>
2023-09-12 13:05:56 +0400

    DEXW-6625: change node url

--- src/configs/configMainnet.ts
+++ src/configs/configMainnet.ts
@@ -2,17 +2,17 @@ import { TConfig } from '../stores/ConfigStore';
 
 export default<TConfig> {
     apiUrl: {
-        node: 'https://nodes.wavesplatform.com',
-        balance: 'https://nodes.wavesplatform.com/addresses/balance',
+        node: 'https://nodes.wavesnodes.com',
+        balance: 'https://nodes.wavesnodes.com/addresses/balance',
         wavesBalance: 'https://nodes.wavesnodes.com/addresses/balance/details',
         assets: 'https://wx.network/api/v1/assets',
         assetsIcons: 'https://wx.network/static/icons/assets',
         rates: 'https://wx.network/api/v1/rates',
-        nodeHeight: 'https://nodes.wavesplatform.com/blocks/height',
+        nodeHeight: 'https://nodes.wavesnodes.com/blocks/height',
         signerWeb: 'https://wx.network/signer/',
         signerCloud: 'https://wx.network/signer-cloud/',
         stateSearch: 'https://wx.network/api/v1/state/search',
-        evaluate: 'https://nodes.wavesplatform.com/utils/script/evaluate',
+        evaluate: 'https://nodes.wavesnodes.com/utils/script/evaluate',
     },
     contracts: {
         factory: '3PJVm7xLPabmYohbnvdgGDYHMwnZxF2x18m',
